e55db28bbed40c0b80dbbef6c76e79c41aba2115
Taiye <taiyeajimati@gmail.com>
2022-07-15 01:38:59 +0400

    css and js update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
-const language = document.querySelector(".language");
-
-for(let i=0; i<language.length; i++){
-    
-    language.addEventListener('click', ()=>{
-       language[i].textContent = "Hello"
-    })
+const cancel = document.querySelector(".cancel");
+const divider = document.querySelector(".divider");
+const input = document.querySelector("input");
 
 
+input.addEventListener('input',search)
+
+function search(){
+if(input.value.length>0){
+    cancel.classList.add('show');
+    divider.classList.add('show');
+    }
 }
+
+cancel.addEventListener('click', canCel)
+
+function canCel(){
+   
+    if(input.value.length>=1){
+     input.value=""
+     cancel.classList.remove('show');
+    divider.classList.remove('show');
+    }  
+
+
+    
+    
+}
+
+// for(let i=0; i<cancel.length; i++){
+    
+//     language.addEventListener('click', ()=>{
+//        language[i].textContent = "Hello"
+//     })
+
+
+// }
