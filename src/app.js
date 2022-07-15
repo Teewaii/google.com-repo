15a68352dce95238752be8750ae395590bb30ac4
Taiye <taiyeajimati@gmail.com>
2022-07-15 18:36:49 +0400

    70% completion

--- src/app.js
+++ src/app.js
@@ -1,37 +1,50 @@
 const cancel = document.querySelector(".cancel");
 const divider = document.querySelector(".divider");
 const input = document.querySelector("input");
+const toolTip = document.querySelector(".tooltip");
+const menuIcon = document.querySelector(".menu");
+const menu = document.querySelector(".main-menu");
+const container = document.querySelector(".container");
 
 
-input.addEventListener('input',search)
 
-function search(){
-if(input.value.length>0){
-    cancel.classList.add('show');
-    divider.classList.add('show');
+
+input.addEventListener('input', search)
+
+function search() {
+    if (input.value.length > 0) {
+        cancel.classList.add('show');
+        divider.classList.add('show');
     }
 }
 
 cancel.addEventListener('click', canCel)
 
-function canCel(){
-   
-    if(input.value.length>=1){
-     input.value=""
-     cancel.classList.remove('show');
-    divider.classList.remove('show');
-    }  
+function canCel() {
 
-
-    
-    
+    if (input.value.length >= 1) {
+        input.value = ""
+        cancel.classList.remove('show');
+        divider.classList.remove('show');
+    }
 }
 
-// for(let i=0; i<cancel.length; i++){
-    
-//     language.addEventListener('click', ()=>{
-//        language[i].textContent = "Hello"
-//     })
+cancel.addEventListener('mouseover', () => {
+    toolTip.classList.toggle('show')
+})
+
+cancel.addEventListener('mouseleave', () => {
+    toolTip.classList.toggle('hide')
+})
+
+menuIcon.addEventListener('click', largeMenu)
+
+function largeMenu() {
+    menu.classList.toggle('show')
+}
 
 
-// }
+// container.addEventListener('click', (event) => {
+//     if(menu!==event.target)
+//     menu.classList.toggle('hide')
+// })
